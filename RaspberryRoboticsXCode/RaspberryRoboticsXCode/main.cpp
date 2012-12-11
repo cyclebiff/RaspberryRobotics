3a55c72dc8a3136daeeedd8d81211d59708936ef
Richard Jesus Dominguez <cyclebiff@gmail.com>
2012-12-11 00:24:17 -0800

    Testing Some Stuff

---
+++ RaspberryRoboticsXCode/RaspberryRoboticsXCode/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  RaspberryRoboticsXCode
+//
+//  Created by Richard Dominguez on 12/11/12.
+//  Copyright (c) 2012 Richard Dominguez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
